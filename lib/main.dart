9ad7250410e464105bafdc24aad67d2d26b6a681
s1270146 <s1270146@gmail.com>
2023-06-02 17:15:59 +0900

    通常画面に変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:portfolio_2023/page/home_page.dart';
