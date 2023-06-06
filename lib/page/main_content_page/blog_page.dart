c40791fdb24d59b256f082aa671cbc72610b95f1
s1270146 <s1270146@gmail.com>
2023-06-06 19:07:07 +0900

    記事一覧 他のページにも反映

--- lib/page/main_content_page/blog_page.dart
+++ lib/page/main_content_page/blog_page.dart
@@ -1,12 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:portfolio_2023/component/profile/profile_title.dart';
+import 'package:portfolio_2023/component/variable_provider.dart';
+import 'package:portfolio_2023/model/article.dart';
 
-class BlogPage extends StatelessWidget {
+class BlogPage extends ConsumerWidget {
   const BlogPage({super.key});
-
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Text("Blog"),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final List<Article> articles = [
+      Article(
+        title: 'テスト1',
+        createDate: DateTime.now(),
+        imagePath: "images/back.jpg",
+        overview:
+            'tttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt',
+      ),
+      Article(
+        title: 'テスト2',
+        createDate: DateTime.now(),
+        imagePath: "images/back.jpg",
+        overview: 'tttttttttttttttttttttttttttttttttttttttttttt',
+      ),
+      Article(
+        title: 'テスト3',
+        createDate: DateTime.now(),
+        imagePath: "images/back.jpg",
+        overview: 'tttttttttttttttttttttttttttttttttttttttttttt',
+      ),
+      Article(
+        title: 'テスト4',
+        createDate: DateTime.now(),
+        imagePath: "images/back.jpg",
+        overview: 'tttttttttttttttttttttttttttttttttttttttttttt',
+      ),
+    ];
+    final mediumSize = ref.watch(mediumSizeProvider);
+    final widgetWidth = mediumSize < MediaQuery.of(context).size.width
+        ? 600.0
+        : MediaQuery.of(context).size.width / 6 * 5;
+    return SingleChildScrollView(
+      child: Center(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 10, bottom: 10),
+              child:
+                  ProfileTitle(profileTitleWidth: widgetWidth, title: 'Blog'),
+            ),
+            for (int i = 0; i < articles.length; i++) ...{
+              Container(
+                margin: const EdgeInsets.all(20),
+                child: articles[i].articleTitle(i, widgetWidth, ref),
+              )
+            }
+          ],
+        ),
+      ),
     );
   }
 }
